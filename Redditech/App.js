f9414262257686b70a88bfdcbc3c169d3ea7467d
dreuxfl <90611235+dreuxfl@users.noreply.github.com>
2022-02-21 11:39:51 +0100

    Can fetch token from securestore

--- Redditech/App.js
+++ Redditech/App.js
@@ -1,16 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
 import Auth from "./Auth";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Auth/>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [token, setToken] = React.useState('');
+
+    React.useEffect(async () => {
+        try {
+            let result = await SecureStore.getItemAsync("token");
+            setToken(result)
+
+        } catch (e) {
+            console.log(e);
+        }
+
+    }, [token] );
+
+    return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app! Token : {token}</Text>
+          <Auth/>
+          <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
--- Redditech/Auth.js
+++ Redditech/Auth.js
@@ -1,19 +1,23 @@
 import * as React from 'react';
 import * as WebBrowser from 'expo-web-browser';
-import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import * as SecureStore from 'expo-secure-store';
+import { makeRedirectUri, useAuthRequest} from 'expo-auth-session';
 import { Button } from 'react-native';
+
 
 WebBrowser.maybeCompleteAuthSession();
 
 const id = 'lDIG15lBBgtfMwJwSp0guQ'; // à changer en fonction de l'id de l'appli sur https://www.reddit.com/prefs/apps
 
-// Endpoint
 const discovery = {
     authorizationEndpoint: 'https://www.reddit.com/api/v1/authorize.compact',
     tokenEndpoint: 'https://www.reddit.com/api/v1/access_token',
 };
 
 export default function Auth() {
+
+    const [token, onChangeToken] = React.useState('');
+
     const [request, response, promptAsync] = useAuthRequest(
         {
             clientId: id,
@@ -28,10 +32,29 @@ export default function Auth() {
 
     React.useEffect(() => {
         if (response?.type === 'success') {
-            const { code } = response.params;
-            console.log(code);
+
+            onChangeToken(response.params.code);
+
+            const saveToken = async () => {
+
+                try {
+                    await SecureStore.setItemAsync("token", token);
+                    let result = await SecureStore.getItemAsync("token");
+                    if (result) {
+                        alert("🔐 Here's your value 🔐 \n" + result);
+                    } else {
+                        alert('No values stored under that key.');
+                    }
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+
+            saveToken();
+
         }
-    }, [response]);
+
+    }, [response,token]);
 
     return (
         <Button
