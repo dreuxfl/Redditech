88ace7b3ccde9d6744078d25620cf883178df4c1
FloANDRE <florian.andre@epitech.eu>
2022-03-01 14:16:25 +0100

    try axios

--- Redditech/components/Login/Login.js
+++ Redditech/components/Login/Login.js
@@ -6,7 +6,8 @@ import {StyleSheet, Button, Text, TouchableOpacity, View, Image } from 'react-na
 
 WebBrowser.maybeCompleteAuthSession();
 
-const id = '1K5vNBLfywoWdVUFsThxXg'; // à changer en fonction de l'id de l'appli sur https://www.reddit.com/prefs/apps
+//const id = '1K5vNBLfywoWdVUFsThxXg'; // à changer en fonction de l'id de l'appli sur https://www.reddit.com/prefs/apps
+const id ='KQj-_0KclqlE6l1Mwv3ABA'
 
 const discovery = {
     authorizationEndpoint: 'https://www.reddit.com/api/v1/authorize.compact',
