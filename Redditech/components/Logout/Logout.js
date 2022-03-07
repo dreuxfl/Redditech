5976cf31c676a55a067bd22cbcf2dda61f010e2b
FloANDRE <florian.andre@epitech.eu>
2022-03-07 11:19:01 +0100

    design profile

--- Redditech/components/Logout/Logout.js
+++ Redditech/components/Logout/Logout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as SecureStore from "expo-secure-store";
-import {Alert, Button, StyleSheet, View} from "react-native";
+import {Alert, Button, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 
 export default function Logout() {
 
@@ -16,21 +16,38 @@ export default function Logout() {
         }
     }
     return (
-        <View style={styles.logout}>
-            <Button
-                title="Logout"
-                onPress={() => {
-                    deleteRedditToken();
-            }}/>
+        <View style={styles.position}>
+            <TouchableOpacity
+                style={styles.logout}
+                onPress={() => {deleteRedditToken()}}
+                >
+                <Text style={styles.text_datas}>Logout</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     logout: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        borderColor:'#35424a',
+        backgroundColor:'#f85f6a',
+        borderWidth:2,
+        borderRadius:53,
+        padding: 6,
+        width:'48%',
+        alignItems:'center'
+    },
+    text_datas:{
+        color:'white',
+        fontSize:12,
+        margin:4,
+    },
+    position:{
+        flexDirection:'row',
+        alignItems:'center',
+        padding: 6,
+        marginHorizontal:10,
+        width:'90%',
+        justifyContent:'space-between'
     },
 });
--- Redditech/screens/Profile/Profile.js
+++ Redditech/screens/Profile/Profile.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image, ScrollView, Button, TouchableOpacity} from 'react-native';
 import * as SecureStore from "expo-secure-store";
 import * as React from "react";
-
+import moment from "moment";
+import Logout from "../../components/Logout/Logout";
 const REDDIT_API = "https://oauth.reddit.com/api/v1"
 const USER_AGENT = "cringeApp.client by FloaNDR13009" //à modifier en fonction de votre utilisateur et client reddit
 //const USER_AGENT = "sadcringe.client by redditech_sadcringe" //à modifier en fonction de votre utilisateur et client reddit
@@ -15,6 +16,7 @@ export default function Profile({ navigation }) {
     const [avatar, setAvatar] = React.useState(null);
     const [friends, setFriends] = React.useState(null);
     const [karma, setKarma] = React.useState(null);
+    const [description, setDescription] = React.useState(null);
 
     const fetchToken = () => {
         return new Promise(async (resolve, reject) => {
@@ -43,7 +45,8 @@ export default function Profile({ navigation }) {
             setFriends(response.data.num_friends)
             setAvatar(response.data.snoovatar_img);
             setKarma(response.data.total_karma);
-            
+            setDescription(response.data.subreddit.public_description)
+
         }).catch((error) => {
             console.log(error)
         });
@@ -69,15 +72,26 @@ export default function Profile({ navigation }) {
 
 
     return (
-        <View style={styles.profile}>
-            <Text>Something went wrong... </Text>
-            <Text>username:{username}</Text>
-            <Text>cake:{createdAt}</Text>
-            <Text>Friends:{friends}</Text>
-            <Text>karma:{karma}</Text>
-            <Image source={{uri: avatar}}/>
+            <View style={styles.profile}>
+                <View><Image style={styles.img_avatar} source={{uri: avatar}}/></View>
 
-        </View>
+                <View style={styles.position}><View style={styles.input_datas }><Text style={styles.text_datas}>Name : {username}</Text></View></View>
+                <View style={styles.position}>
+                    <View style={styles.input_datas2}><Text style={styles.text_datas}>Friends : {friends}</Text></View>
+                    <View style={styles.input_datas2}><Text style={styles.text_datas}>Karma : {karma}</Text></View>
+                </View>
+                <View style={styles.position}><View style={styles.input_datas}><Text style={styles.text_datas}>Birthday cake : {moment.unix(parseInt(createdAt)).format("DD/MM/YYYY")}</Text></View></View>
+                <View style={styles.position}><View style={styles.input_datas3}><Text style={styles.text_datas}>Description :</Text>
+                    <Text style={styles.text_datas}> {description}</Text></View>
+                </View>
+                <View style={styles.position}>
+                    <TouchableOpacity
+                        style={styles.input_button} >
+                        <Text style={styles.text_datas}>Settings</Text>
+                    </TouchableOpacity>
+                    <Logout/>
+                </View>
+            </View>
         )
 }
 const styles = StyleSheet.create({
@@ -87,5 +101,62 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         color:'white',
-    }
+    },
+    img_avatar: {
+        width: 127,
+        height: 200,
+        marginBottom: 10,
+        marginTop: 25,
+    },
+    input_datas:{
+        backgroundColor:'#118ab2',
+        borderRadius:53,
+        padding: 6,
+        width:'100%',
+        alignItems:'center',
+    },
+    input_datas2:{
+        backgroundColor:'#118ab2',
+        borderRadius:53,
+        padding: 6,
+        width:'48%',
+    },
+    input_datas3:{
+        backgroundColor:'#118ab2',
+        borderRadius:13,
+        padding: 10,
+        width:'100%',
+    },
+    input_button:{
+        backgroundColor:'#2c2f33',
+        borderColor:'#094e65',
+        borderWidth:2,
+        borderRadius:53,
+        padding: 6,
+        width:'48%',
+        alignItems:'center',
+    },
+    logout:{
+        backgroundColor:'#f85f6a',
+        borderColor:'#35424a',
+        borderWidth:2,
+        borderRadius:53,
+        padding: 6,
+        width:'48%',
+        alignItems:'center'
+    },
+    text_datas:{
+        color:'white',
+        fontSize:12,
+        margin:4,
+    },
+    position:{
+        flexDirection:'row',
+        alignItems:'center',
+        padding: 6,
+        marginHorizontal:10,
+        width:'90%',
+        justifyContent:'space-between'
+    },
+
 });
