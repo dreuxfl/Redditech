03dca0d16368f1fb9899ee16fd31b43df6fc0ec8
dreuxfl <90611235+dreuxfl@users.noreply.github.com>
2022-03-14 13:01:29 +0100

    Filters done

--- Redditech/screens/Profile/Profile.js
+++ Redditech/screens/Profile/Profile.js
@@ -7,8 +7,8 @@ import Logout from "../../components/Logout/Logout";
 import Settings from "../../stacks/Settings/Settings";
 
 const REDDIT_API = "https://oauth.reddit.com/api/v1"
-const USER_AGENT = "cringeApp.client by FloaNDR13009" //à modifier en fonction de votre utilisateur et client reddit
-//const USER_AGENT = "sadcringe.client by redditech_sadcringe" //à modifier en fonction de votre utilisateur et client reddit
+//const USER_AGENT = "cringeApp.client by FloaNDR13009" //à modifier en fonction de votre utilisateur et client reddit
+const USER_AGENT = "sadcringe.client by redditech_sadcringe" //à modifier en fonction de votre utilisateur et client reddit
 
 export default function Profile({ navigation }) {
 
